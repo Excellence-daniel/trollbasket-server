d5d0fe69f5ce34ec60790509c4db0ca5a0ccce55
Excellence Oyeniran <oyeniranexcellence>
2021-05-18 19:18:57 +0100

    removed cors call

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(morgan('dev'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.options('*', cors())
+// app.options('*', cors())
 app.use(cors())
 
 app.listen(process.env.PORT || port, () => {
