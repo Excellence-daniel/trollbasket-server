a2f3934c059cead820ebddc2d98f39f8184d5835
Excellence Oyeniran <oyeniranexcellence>
2021-05-18 19:46:27 +0100

    updated cors issue

--- index.js
+++ index.js
@@ -24,8 +24,17 @@ app.use(morgan('dev'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// app.options('*', cors())
+app.options('*', cors())
 app.use(cors())
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000') // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 
 app.listen(process.env.PORT || port, () => {
   console.log('Server started work on ' + port)
